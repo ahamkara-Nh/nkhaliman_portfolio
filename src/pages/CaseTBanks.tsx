7dbc45a22fd2cc107801186b577cd5d720edb0d6
ahamkara-Nh <haliman.ru@gmail.com>
2025-09-01 02:15:12 +0300

    Icon adjustment

--- src/pages/CaseTBanks.tsx
+++ src/pages/CaseTBanks.tsx
@@ -583,7 +583,8 @@ export default function CaseTBanks() {
                   <div className={styles['case-tbanks__mobile-banner-content']}>
                     <div className={styles['case-tbanks__mobile-banner-text']}>
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15M7 10L12 15M12 15L17 10M12 15V3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        <circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="2"/>
                       </svg>
                       <span>Интерактивный прототип интерфейса</span>
                     </div>
@@ -652,7 +653,8 @@ export default function CaseTBanks() {
                   <div className={styles['case-tbanks__mobile-banner-content']}>
                     <div className={styles['case-tbanks__mobile-banner-text']}>
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15M7 10L12 15M12 15L17 10M12 15V3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        <circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="2"/>
                       </svg>
                       <span>Figma презентация проекта</span>
                     </div>

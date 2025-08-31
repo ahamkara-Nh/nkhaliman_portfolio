3047be958f6ea6a00670ccf512913b450117da02
ahamkara-Nh <haliman.ru@gmail.com>
2025-09-01 02:11:23 +0300

    Load optimisation

--- src/pages/CaseTBanks.tsx
+++ src/pages/CaseTBanks.tsx
@@ -578,7 +578,24 @@ export default function CaseTBanks() {
             </p>
 
             <div ref={prototypeRef} className={styles['case-tbanks__prototype-container']}>
-              {isPrototypeInView ? (
+              {isMobile && !isPrototypeLoaded ? (
+                <div className={styles['case-tbanks__mobile-banner']}>
+                  <div className={styles['case-tbanks__mobile-banner-content']}>
+                    <div className={styles['case-tbanks__mobile-banner-text']}>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15M7 10L12 15M12 15L17 10M12 15V3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                      <span>Интерактивный прототип интерфейса</span>
+                    </div>
+                    <button
+                      className="case-lowfodmap__telegram-button"
+                      onClick={() => setIsPrototypeLoaded(true)}
+                    >
+                      Загрузить прототип
+                    </button>
+                  </div>
+                </div>
+              ) : isPrototypeInView || isPrototypeLoaded ? (
                 <iframe
                   src="https://embed.figma.com/proto/abaStT5Mi9iiEgAs4sdcrK/%D0%A2%D0%B5%D0%94%D0%BE?page-id=0%3A1&node-id=734-32&viewport=1424%2C482%2C0.11&scaling=scale-down&content-scaling=fixed&starting-point-node-id=734%3A32&embed-host=share&footer=false&theme=dark"
                   allowFullScreen
@@ -630,7 +647,24 @@ export default function CaseTBanks() {
               </a>, чтобы открыть презентацию
             </p>
             <div ref={outcomesRef} className={styles['case-tbanks__outcomes-iframe-container']}>
-              {isOutcomesInView ? (
+              {isMobile && !isOutcomesLoaded ? (
+                <div className={styles['case-tbanks__mobile-banner']}>
+                  <div className={styles['case-tbanks__mobile-banner-content']}>
+                    <div className={styles['case-tbanks__mobile-banner-text']}>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15M7 10L12 15M12 15L17 10M12 15V3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                      <span>Figma презентация проекта</span>
+                    </div>
+                    <button
+                      className="case-lowfodmap__telegram-button"
+                      onClick={() => setIsOutcomesLoaded(true)}
+                    >
+                      Загрузить презентацию
+                    </button>
+                  </div>
+                </div>
+              ) : isOutcomesInView || isOutcomesLoaded ? (
                 <iframe
                   style={{border: '1px solid rgba(0, 0, 0, 0.1)'}}
                   width="800"
